eafdbbd64698ea7e35e24176a8b61d5904d42cbc
YifanXu <yifanxu00@outlook.com>
2024-11-01 14:54:57 -0400

    remove apollo

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,23 @@
-import express from 'express';
-import http from 'http';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import { createApolloMiddleware } from './graphql/graphql';
+import express from 'express'
+import http from 'http'
+import cors from 'cors'
 
 async function main() {
-	const app = express();
-	const httpServer = http.createServer(app);
+	const app = express()
+	const httpServer = http.createServer(app)
 
 	// Setup middlewares
-	app.use(
-		'/graphql',
-		cors(),
-		bodyParser.json(),
-		await createApolloMiddleware(httpServer),
-	);
+	app.use(cors())
+
+	app.get('/', (req, res) => {
+		res.send('Hello World!')
+	})
 
 	// Start Server
 	await new Promise<void>(resolve => httpServer.listen(4000, resolve))
 
 	// Signal ready
-	console.log(`🚀 Server ready at http://localhost:4000/graphql`);
+	console.log(`🚀 Server ready at http://localhost:4000`)
 }
 
 main()
