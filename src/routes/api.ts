4442c4ebc44ef040cc7666589753dccbeb6fdcda
YifanXu <yifanxu00@outlook.com>
2024-11-08 13:37:22 -0500

    add api.ts

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,16 +1,7 @@
-import express, { Request, Response } from 'express'
+import express from 'express'
 
 const router = express.Router()
 
-/**
- * @openapi
- * /:
- *   get:
- *     description: Welcome to swagger-jsdoc!
- *     responses:
- *       200:
- *         description: Returns a mysterious string.
- */
 router.get('/', (req, res) => {
 	res.send('Hello world!')
 })
